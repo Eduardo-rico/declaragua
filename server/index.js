f993ac459080643822a9af4876832eca80772c5d
Eduardo Rico <erico@intelimetrica.com>
2021-04-23 21:59:06 -0500

    otra cosa!

--- server/index.js
+++ server/index.js
@@ -49,6 +49,17 @@ app.use((req, res, next) => {
 		'Access-Control-Allow-Headers',
 		'Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Origin,authorization'
 	);
+
+	req.header('Access-Control-Allow-Origin', '*');
+	req.header(
+		'Access-Control-Allow-Methods',
+		'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+	);
+	req.header(
+		'Access-Control-Allow-Headers',
+		'Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Origin,authorization'
+	);
+
 	next();
 });
 
