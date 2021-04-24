d8978bdf6ada4750e1a61b00240be4ed52e31e64
Eduardo Rico <erico@intelimetrica.com>
2021-04-23 21:57:14 -0500

    se agrega authorization al header permitido

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 	);
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Origin'
+		'Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Origin,authorization'
 	);
 	next();
 });
