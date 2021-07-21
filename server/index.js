85ad2cf7c44daed4d3b4f54abe9da658dc246624
Eduardo Rico <erico@intelimetrica.com>
2021-07-20 19:37:03 -0500

    solved cors

--- server/index.js
+++ server/index.js
@@ -13,22 +13,22 @@ const usuarioInternosRouter = require('./routes/usuariosInternos.routes');
 
 //inicializaciones
 mongoose.connect(
-	process.env.MONGODB_URI,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-		useCreateIndex: true,
-	},
-	() => {
-		console.log('db conectada');
-		app.listen(port, () => {
-			console.log(`Servidor arriba en http://localhost:${port}/`);
-			const timeElapsed = Date.now();
-			const today = new Date(timeElapsed);
-			console.log(`La fecha: ${today.toUTCString()}`);
-		});
-	}
+        process.env.MONGODB_URI,
+        {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true,
+        },
+        () => {
+                console.log('db conectada');
+                app.listen(port, () => {
+                        console.log(`Servidor arriba en http://localhost:${port}/`);
+                        const timeElapsed = Date.now();
+                        const today = new Date(timeElapsed);
+                        console.log(`La fecha: ${today.toUTCString()}`);
+                });
+        }
 );
 
 const app = express();
@@ -39,20 +39,21 @@ const port = 3001;
 //middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(helmet());
-app.use(
-	cors({
-		origin: '*',
-		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-	})
-);
-//rutas
-app.use('/', usuarioRouter);
+app.use(express.urlencoded({ extended: true }))
+
+// use it before all route definitions
+app.all('/*',function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://ricosotomayor.com");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PATCH,POST,PUTS");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.options('*', cors())
+app.use('/',usuarioRouter);
 app.use('/plataforma', usuarioInternosRouter);
 
 //listen
-
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + 'index.html');
+        res.sendFile(__dirname + 'index.html');
 });
