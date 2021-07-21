c585ab21c9477bf15a44063c613a0026a45c8e44
Eduardo Rico <erico@intelimetrica.com>
2021-07-20 21:43:49 -0500

    change folderrr

---
+++ server/cliente/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components';
+import FormSignIn from './components/FormSignIn/FormSignIn';
+import GlobalStyle from './components/GlobalStyle/GlobalStyle';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Body from './components/Body/Body';
+
+const Container = styled.div`
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+function App() {
+	return (
+		<Router>
+			<Container>
+				<GlobalStyle />
+				<Switch>
+					<Route exact path="/" component={FormSignIn} />
+					<Route exact path="/plat" component={Body} />
+				</Switch>
+			</Container>
+		</Router>
+	);
+}
+
+export default App;
